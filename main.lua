791476ea974c122eaf07e01e8320ed0df663cbb1
Sarthak0714 <sarthak.t1403@gmail.com>
2024-12-30 00:16:26 +0530

    projectile start

--- main.lua
+++ main.lua
@@ -1,12 +1,20 @@
 local love = require("love")
 
+Player = {}
+
+Projectile = {}
+
 function love.load()
     love.window.setMode(800, 400, { resizable = true, vsync = 0 })
     love.window.maximize()
-    player = {}
+    player = Player
     player.speed = 2
     player.x = love.graphics.getWidth() / 2
-    player.y = love.graphics.getHeight() - (love.graphics.getHeight() * 0.1)
+    player.y = love.graphics.getHeight() - (0.05 * love.graphics.getHeight())
+    ally_beam = {}
+    ally_beam.speed = 1
+    ally_beam.x = player.x
+    ally_beam.y = player.y
 end
 
 function love.update(dt)
@@ -16,8 +24,10 @@ function love.update(dt)
     if love.keyboard.isDown("left") then
         player.x = player.x - player.speed
     end
+    ally_beam.y = ally_beam.y - ally_beam.speed
 end
 
 function love.draw()
     love.graphics.circle("fill", player.x, player.y, 20)
+    love.graphics.rectangle("fill", ally_beam.x, ally_beam.y, 4, 8)
 end
